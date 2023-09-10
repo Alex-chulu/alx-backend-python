5ef630f0e3c36a7c676b91556c3faf3a3b19413b
Alex-chulu <chulualexander@gmail.com>
2023-09-10 20:33:29 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import math
+
+def floor(n: float) -> int:
+    """
+    Returns the floor (largest integer less than or equal to) of a given float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        int: The floor value of the input float.
+    """
+    return math.floor(n)
+
