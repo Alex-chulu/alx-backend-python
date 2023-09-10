6152228afafe5b58eea26501b9e719ec52a2647c
Alex-chulu <chulualexander@gmail.com>
2023-09-10 20:22:46 +0000

    task 1 fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 def concat(str1: str, str2: str) -> str:
     """
     Concatenates two input strings and returns the result.
@@ -10,4 +12,3 @@ def concat(str1: str, str2: str) -> str:
         str: The concatenated string of str1 and str2.
     """
     return str1 + str2
-
