bbd77482d8d26b139a6667b2a4b2abbd6ba78600
Alex-chulu <chulualexander@gmail.com>
2023-10-08 20:39:55 +0000

    async

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
-"""
-Async Comprehension Example
-
-This module defines an asynchronous coroutine called async_comprehension 
-that collects 10 random numbers using an async comprehension over async_generator.
-
-Usage:
-1. Run this module directly to see the collected random numbers.
-2. You can also import and use async_comprehension in other modules.
-
-Example:
-    $ python3 async_comprehension.py
-"""
-from typing import List
+'''Task 2's module.
+'''
 import asyncio
+from typing import List
+from random import random
 
-async_generator = __import__('0-async_generator').async_generator
-
+async def async_generator() -> float:
+    '''Asynchronous generator that yields 10 random numbers.'''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random() * 100
 
 async def async_comprehension() -> List[float]:
-    """coroutine will collect random numbers using an async
-    comprehensing"""
-    return [y async for y in async_generator()]
+    '''Collects 10 random numbers using async comprehensions.'''
+    return [rand async for rand in async_generator()]
+
+# Example usage:
+async def main():
+    '''Main coroutine to run async_comprehension.'''
+    random_numbers = await async_comprehension()
+    print(random_numbers)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
