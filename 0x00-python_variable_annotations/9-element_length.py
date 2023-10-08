64b812e7719ad603a5d6a1eebc6e2bed428d180e
Alex-chulu <chulualexander@gmail.com>
2023-10-08 19:52:33 +0000

    element

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,19 +4,15 @@ from typing import List, Tuple
 
 def element_length(lst: List[str]) -> List[Tuple[str, int]]:
     """
-    Calculate the length of each element in the input list and return a list
-    of tuples containing each element and its corresponding length.
+    Compute the length of each string in the given list.
 
     Args:
-        lst (List[str]): The list of strings to calculate lengths for.
+        lst (List[str]): A list of strings.
 
     Returns:
-        List[Tuple[str, int]]: A list of tuples where the first element is a
-        string from the input list, and the second element is its length.
-
-    Example:
-        >>> element_length(["apple", "banana", "cherry"])
-        [('apple', 5), ('banana', 6), ('cherry', 6)]
+        List[Tuple[str, int]]: A list of tuples where the first element
+        is a string from the input list, and the second element is the
+        length of that string.
     """
     return [(i, len(i)) for i in lst]
 
