7127d2597ad461460e621b4a2b5ac21210535052
Alex-chulu <chulualexander@gmail.com>
2023-10-08 20:30:34 +0000

    type checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python3
-'''Task 12's module.
-'''
-from typing import Tuple
+"""
+Zooms in on an array by repeating its elements by a factor
+"""
+from typing import List, Tuple
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
-    '''Zooms in on an array by repeating its elements by a factor.
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+    """Zooms in on an array by repeating its elements by a factor.
+
     Args:
         lst (Tuple[int, ...]): The input array.
         factor (int, optional): The zoom factor. Defaults to 2.
 
     Returns:
-        Tuple[int, ...]: The zoomed-in array.
-    '''
-    zoomed_in: Tuple[int, ...] = tuple(
-        item for item in lst
+        List[int]: The zoomed-in array.
+    """
+    zoomed_in: List[int] = [
+        x for x in lst
         for _ in range(factor)
-    )
+    ]
     return zoomed_in
 
-array = (12, 72, 91)
 
-zoom_2x = zoom_array(array)
+array: Tuple[int, ...] = (12, 72, 91)
 
-zoom_3x = zoom_array(array, 3)
+zoom_2x: List[int] = zoom_array(array)
+
+zoom_3x: List[int] = zoom_array(array, 3)
 
