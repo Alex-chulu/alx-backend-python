7807c0a2013a672332076f797a4666140592be64
Alex-chulu <chulualexander@gmail.com>
2023-10-08 19:55:40 +0000

    safe first

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,23 +1,17 @@
-#!/usr/bin/env python3
+from typing import List, Any, Union
 
-"""
-Module: my_module
-This module provides a function for obtaining the first element of an iterable.
-"""
-
-from typing import Iterable, Any
-
-def safe_first_element(lst: Iterable[Any]) -> Any:
+def safe_first_element(lst: List[Any]) -> Union[None, Any]:
     """
-    Get the first element of an iterable if it exists.
+    Safely retrieve the first element of a list if it exists.
 
     Args:
-        lst (Iterable[Any]): An iterable of elements.
+        lst (List[Any]): A list of elements of any type.
 
     Returns:
-        Any: The first element of the iterable, or None if the iterable is empty.
+        Union[None, Any]: The first element of the list if it exists, else None.
     """
     if lst:
         return lst[0]
     else:
         return None
+
