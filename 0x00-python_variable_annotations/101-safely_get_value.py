8d62b73f3d2a54efcd6b83a024f45cf480cd17c6
Alex-chulu <chulualexander@gmail.com>
2023-10-08 20:14:45 +0000

    safely get value

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
+'''Task 11's module.
+'''
 
-"""
-safely_get_value - Safely gets a value from a dictionary.
+from typing import Dict, TypeVar, Optional
 
-This function safely retrieves a value from a dictionary by providing a key and an optional default value.
-If the key exists in the dictionary, the corresponding value is returned. Otherwise, the default value (which is None by default) is returned.
-
-Parameters:
-- dct (dict): The dictionary to retrieve the value from.
-- key: The key to look up in the dictionary.
-- default (optional): The default value to return if the key is not found. Defaults to None.
-
-Returns:
-- The value associated with the given key if it exists in the dictionary, or the default value.
-
-"""
-
-from typing import TypeVar, Dict, Optional
-
-K = TypeVar('K')  # Key type
-V = TypeVar('V')  # Value type
+K = TypeVar('K')  # TypeVar for the dictionary key
+V = TypeVar('V')  # TypeVar for the return value
 
 def safely_get_value(dct: Dict[K, V], key: K, default: Optional[V] = None) -> V:
+    '''Safely retrieves a value from a dictionary.
+    
+    Args:
+        dct (Dict[K, V]): The dictionary to retrieve the value from.
+        key (K): The key to search for in the dictionary.
+        default (Optional[V], optional): The default value to return if the key is not found.
+    
+    Returns:
+        V: The value associated with the key in the dictionary, or the default value if not found.
+    '''
     if key in dct:
         return dct[key]
     else:
